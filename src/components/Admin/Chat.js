b76a4da737ffe02d02a1670176875f44891a9472
Didi Aryadi <108334135+Qua-Qu@users.noreply.github.com>
2023-07-28 02:51:44 +0700

    Update Chat.js

--- src/components/Admin/Chat.js
+++ src/components/Admin/Chat.js
@@ -6,7 +6,7 @@ import { createMessage, getListMessage } from "../../services/API/messengerApi";
 import queryString from "query-string";
 import { getListUser } from "../../services/API/userApi";
 import moment from "moment";
-const socket = io("https://samshanbackend.railway.internal:3000");
+const socket = io("samshanbackend-production.up.railway.app:3000");
 
 export default function Chat() {
   const [load, setLoad] = useState(false);
